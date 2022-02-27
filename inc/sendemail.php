f98f85f963e7c38ccff2e1c3ee8e4c30b5818e85
Nchimunya Munyama <munyama01@gmail.com>
2022-02-27 18:16:28 +0200

    made several changes

--- inc/sendemail.php
+++ inc/sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "mail@mail.com" );
+define( "RECIPIENT_NAME", "Boa Daakye Foundation" );
+define( "RECIPIENT_EMAIL", "mobilelibraryproject@gmail.com" );
 
 // Read the form values
 $success = false;
